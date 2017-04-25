af345d79d3f8dbdb3e201e666284de9021c35e0a
TheHockeyist <thockeyist@gmail.com>
2017-04-25 12:12:51 -0500

    Version 1.
    Does this work?

--- code.py
+++ code.py
@@ -2,8 +2,24 @@ beverage = raw_input("What beverage is in the song?")
 third_line = raw_input("What should the third line be? E.g. If one of those bottles should happen to fall,")
 
 set bottles = 99
-print bottles "bottles of "beverage" on the wall,
-print bottles "bottles of "beverage".
+repeat 97
+  print bottles "bottles of "beverage" on the wall,"
+  print bottles "bottles of "beverage"."
+  print third_line
+  set bottles = bottles - 1
+  print bottles "bottles of "beverage" on the wall!"
+print bottles "bottles of "beverage" on the wall,"
+print bottles "bottles of "beverage"."
 print third_line
 set bottles = bottles - 1
-print bottles "bottles of "beverage" on the wall,
+print bottle "bottle of "beverage" on the wall!" # One bottle is a special case.
+print bottles "bottle of "beverage" on the wall,"
+print bottles "bottle of "beverage"."
+print third_line
+set bottles = bottles - 1
+print bottles "bottles of "beverage" on the wall!"
+print bottles "bottles of "beverage" on the wall,"
+print bottles "bottles of "beverage" on the wall."
+print "Go to the store and buy some more,"
+set bottles = 99
+print bottles "bottles of "beverage" on the wall!"
