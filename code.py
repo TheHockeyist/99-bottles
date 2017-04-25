c95b511a5cef1fcd9ddd12614572c8f04a489133
TheHockeyist <thockeyist@gmail.com>
2017-04-25 12:04:44 -0500

    Initial code.
    Rough draft.

---
+++ code.py
@@ -0,0 +1,7 @@
+beverage = raw_input("What beverage?")
+third_line = raw_input("What should the third line be?")
+
+print "99 bottles of beverage on the wall,
+print "99 bottles of beverage,
+print "third_line"
+print "98 bottles of beverage on the wall,
