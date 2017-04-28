02ac4c716239656cf01940dbf8b46be627684369
TheHockeyist <thockeyist@gmail.com>
2017-04-28 11:59:19 -0500

    Take 3.
    Commas.

--- code.py
+++ code.py
@@ -3,23 +3,23 @@ third_line = raw_input("What should the third line be? E.g. If one of those bott
 
 bottles = 99
 for i in range(97)
-  print bottles "bottles of "beverage" on the wall,"
-  print bottles "bottles of "beverage"."
+  print bottles ,"bottles of ,"beverage", on the wall,"
+  print bottles ,"bottles of ,"beverage",."
   print third_line
-  set bottles = bottles - 1
-  print bottles "bottles of "beverage" on the wall!"
-print bottles "bottles of "beverage" on the wall,"
-print bottles "bottles of "beverage"."
+  bottles = bottles - 1
+  print bottles ,"bottles of ,"beverage", on the wall!"
+print bottles ,"bottles of ,"beverage", on the wall,"
+print bottles ,"bottles of ,"beverage",."
 print third_line
-set bottles = bottles - 1
-print bottle "bottle of "beverage" on the wall!" # One bottle is a special case.
-print bottles "bottle of "beverage" on the wall,"
-print bottles "bottle of "beverage"."
+bottles = bottles - 1
+print bottles ,"bottle of ,"beverage", on the wall!" # One bottle is a special case.
+print bottles ,"bottle of ,"beverage" on the wall,"
+print bottles ,"bottle of ,"beverage",."
 print third_line
-set bottles = bottles - 1
-print bottles "bottles of "beverage" on the wall!"
-print bottles "bottles of "beverage" on the wall,"
-print bottles "bottles of "beverage" on the wall."
+bottles = bottles - 1
+print bottles ,"bottles of ,"beverage", on the wall!"
+print bottles ,"bottles of ,"beverage", on the wall,"
+print bottles ,"bottles of ,"beverage",."
 print "Go to the store and buy some more,"
-set bottles = 99
-print bottles "bottles" of "beverage" on the wall!"
+bottles = 99
+print bottles ,"bottles of ,"beverage", on the wall!"
