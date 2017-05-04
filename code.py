37899f825a8baa813c20a57073e1aa1bb650310c
TheHockeyist <thockeyist@gmail.com>
2017-05-04 14:08:17 -0500

    This should work now.
    What was I thinking when I coded the strings?

--- code.py
+++ code.py
@@ -3,23 +3,23 @@ third_line = raw_input("What should the third line be? E.g. If one of those bott
 
 bottles = 99
 while (bottles > 2):
-  print bottles," bottles of ,"beverage", on the wall,"
-  print bottles," bottles of ,"beverage",."
+  print "%d bottles of %s on the wall," % (bottles, beverage)
+  print "%d bottles of %s." % (bottles, beverage)
   print third_line
   bottles = bottles - 1
-  print bottles," bottles of ,"beverage", on the wall!"
-print bottles," bottles of ,"beverage", on the wall,"
-print bottles," bottles of ,"beverage",."
+  print "%d bottles of %s on the wall!" % (bottles, beverage)
+print "%d bottles of %s on the wall," % (bottles, beverage)
+print "%d bottles of %s." % (bottles, beverage)
 print third_line
 bottles = bottles - 1
-print bottles," bottle of ,"beverage", on the wall!" # One bottle is a special case.
-print bottles," bottle of ,"beverage" on the wall,"
-print bottles," bottle of ,"beverage",."
+print "%d bottle of %s on the wall!" % (bottles, beverage) # One bottle is a special case.
+print "%d bottle of %s on the wall," % (bottles, beverage)
+print "%d bottle of %s." % (bottles, beverage)
 print third_line
 bottles = bottles - 1
-print bottles," bottles of ,"beverage", on the wall!"
-print bottles," bottles of ,"beverage", on the wall,"
-print bottles," bottles of ,"beverage",."
+print "%d bottles of %s on the wall!" % (bottles, beverage
+print "%d bottles of %s on the wall," % (bottles, beverage)
+print "%d bottles of %s." % (bottles, beverage)
 print "Go to the store and buy some more,"
 bottles = 99
-print bottles," bottles of ,"beverage", on the wall!"
+print "%d bottles of %s on the wall!" % (bottles, beverage
