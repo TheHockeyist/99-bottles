8450c21d73dd20aff35363edd5c6dce31bfd16d3
TheHockeyist <thockeyist@gmail.com>
2017-05-04 14:01:37 -0500

    Spacing.
    Okay now...

--- code.py
+++ code.py
@@ -3,23 +3,23 @@ third_line = raw_input("What should the third line be? E.g. If one of those bott
 
 bottles = 99
 while (bottles > 2):
-  print bottles ,"bottles of ,"beverage", on the wall,"
-  print bottles ,"bottles of ,"beverage",."
+  print bottles," bottles of ,"beverage", on the wall,"
+  print bottles," bottles of ,"beverage",."
   print third_line
   bottles = bottles - 1
-  print bottles ,"bottles of ,"beverage", on the wall!"
-print bottles ,"bottles of ,"beverage", on the wall,"
-print bottles ,"bottles of ,"beverage",."
+  print bottles," bottles of ,"beverage", on the wall!"
+print bottles," bottles of ,"beverage", on the wall,"
+print bottles," bottles of ,"beverage",."
 print third_line
 bottles = bottles - 1
-print bottles ,"bottle of ,"beverage", on the wall!" # One bottle is a special case.
-print bottles ,"bottle of ,"beverage" on the wall,"
-print bottles ,"bottle of ,"beverage",."
+print bottles," bottle of ,"beverage", on the wall!" # One bottle is a special case.
+print bottles," bottle of ,"beverage" on the wall,"
+print bottles," bottle of ,"beverage",."
 print third_line
 bottles = bottles - 1
-print bottles ,"bottles of ,"beverage", on the wall!"
-print bottles ,"bottles of ,"beverage", on the wall,"
-print bottles ,"bottles of ,"beverage",."
+print bottles," bottles of ,"beverage", on the wall!"
+print bottles," bottles of ,"beverage", on the wall,"
+print bottles," bottles of ,"beverage",."
 print "Go to the store and buy some more,"
 bottles = 99
-print bottles ,"bottles of ,"beverage", on the wall!"
+print bottles," bottles of ,"beverage", on the wall!"
