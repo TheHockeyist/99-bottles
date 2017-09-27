4d89b0b587d13e8cc05873e2f252ecde609d1725
Andrew <ateesdalejr@users.noreply.github.com>
2017-09-26 20:01:28 -0700

    Optimized version.
    Reduced the reuse of formatting strings. Looks less pretty but does the job well.

--- code.py
+++ code.py
@@ -1,24 +1,26 @@
 beverage = raw_input("What beverage is in the song?")
 third_line = raw_input("What should the third line be? E.g. If one of those bottles should happen to fall,")
 bottles = 99
+bob="%d bottles of %s on the wall"
+bob2="%d bottles of %s."
 while (bottles > 2):
-  print "%d bottles of %s on the wall," % (bottles, beverage)
-  print "%d bottles of %s." % (bottles, beverage)
+  print bob+"," % (bottles, beverage)
+  print bob2 % (bottles, beverage)
   print third_line
   bottles = bottles - 1
-  print "%d bottles of %s on the wall!" % (bottles, beverage)
-print "%d bottles of %s on the wall," % (bottles, beverage)
-print "%d bottles of %s." % (bottles, beverage)
+  print bob+"!" % (bottles, beverage)
+print bob+"," % (bottles, beverage)
+print bob2 % (bottles, beverage)
 print third_line
 bottles = bottles - 1
-print "%d bottle of %s on the wall!" % (bottles, beverage) # One bottle is a special case.
-print "%d bottle of %s on the wall," % (bottles, beverage)
-print "%d bottle of %s." % (bottles, beverage)
+print bob+"!" % (bottles, beverage) # One bottle is a special case.
+print bob+"," % (bottles, beverage)
+print bob2 % (bottles, beverage)
 print third_line
 bottles = bottles - 1
-print "%d bottles of %s on the wall!" % (bottles, beverage)
-print "%d bottles of %s on the wall," % (bottles, beverage)
-print "%d bottles of %s." % (bottles, beverage)
+print bob+"!" % (bottles, beverage)
+print bob+"," % (bottles, beverage)
+print bob2 % (bottles, beverage)
 print "Go to the store and buy some more,"
 bottles = 99
-print "%d bottles of %s on the wall!" % (bottles, beverage)
+print bob+"!" % (bottles, beverage)
